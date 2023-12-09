b1f0e419cc5f816c182fde6d18ce4d60882b0d88
MarkyMan4 <markus.hutnik@gmail.com>
2023-12-09 12:20:51 -0600

    day 9

---
+++ day9/part1/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parseLine(text string) []int {
+	vals := strings.Split(text, " ")
+	intVals := []int{}
+	for _, val := range vals {
+		intVal, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+		intVals = append(intVals, intVal)
+	}
+
+	return intVals
+}
+
+func isAllZeros(vals []int) bool {
+	if len(vals) == 0 {
+		return false
+	}
+	for _, val := range vals {
+		if val != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	predictionTotal := 0
+
+	for scanner.Scan() {
+		readings := parseLine(scanner.Text())
+		lastNumsInSeqs := []int{readings[len(readings)-1]}
+		prevDiffs := readings
+
+		for {
+			diffs := []int{}
+			for i := 0; i < len(prevDiffs)-1; i++ {
+				diffs = append(diffs, prevDiffs[i+1]-prevDiffs[i])
+			}
+
+			if isAllZeros(diffs) {
+				// no need to store the slice of all zeros
+				break
+			}
+
+			prevDiffs = diffs
+
+			// only need to store the last value in each sequence since that's the only one that's
+			// needed to make the prediction
+			lastNumsInSeqs = append(lastNumsInSeqs, diffs[len(diffs)-1])
+		}
+
+		prediction := 0
+		for i := len(lastNumsInSeqs) - 1; i >= 0; i-- {
+			prediction = prediction + lastNumsInSeqs[i]
+		}
+
+		predictionTotal += prediction
+	}
+
+	fmt.Println(predictionTotal)
+}
